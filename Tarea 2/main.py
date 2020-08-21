42ee75b7f797c19a5b84888a1acc06fce1ed565b
JOSUÉ DAVID ZEA HERRERA <jdzeaherrera@gmail.com>
2020-08-20 21:32:30 -0600

    Tarea 2

---
+++ Tarea 2/main.py
@@ -0,0 +1,12 @@
+print("************* Archivo xml ******************")
+import LectorXML
+LectorXML
+print("********************************************")
+print("************* Archivo json ******************")
+import LectorJSON
+LectorJSON
+print("********************************************")
+print("************* Archivo csv ******************")
+import LectorCSV
+LectorCSV
+print("********************************************")
